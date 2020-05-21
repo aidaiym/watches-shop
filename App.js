ffa8ce5b531961b12315b33111e23619a69aa7c5
Aidaiym <omurbekovnaaidaiym@gmail.com>
2020-05-21 11:44:45 +0600

    Cleanup

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+function App() {
+    return (
+        <div className='App'>
+            Whatch shop by Aidaiym Omurbekovna
+        </div>
+    );
+}
+export default App;
